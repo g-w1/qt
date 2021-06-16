639b15897579b6feb095a8274a8df4d777911b08
Jacob G-W <jacoblevgw@gmail.com>
2021-06-15 22:09:40 -0400

    add lev distance
    thanks @rafi

--- src/main.zig
+++ src/main.zig
@@ -41,12 +41,13 @@ pub fn main() anyerror!void {
     var start = true;
     while (lines.items.len > 0 or start) {
         while (lines.popOrNull()) |item| {
-            try stdout.print("\n[{d}/{d}] def of `{s}`?\n", .{ correct.items.len, incorrect.items.len + lines.items.len + correct.items.len, item.term });
+            try stdout.print("\n[{d}/{d}] def of `{s}`?\n", .{ correct.items.len, incorrect.items.len + lines.items.len, item.term });
             const answer = (try stdin.readUntilDelimiterOrEofAlloc(gpa, '\n', std.math.maxInt(u16))) orelse return;
             defer gpa.free(answer);
             if (std.mem.trim(u8, answer, " \n").len == 0)
-                continue;
-            if (std.mem.eql(u8, std.mem.trim(u8, answer, " \t"), item.def)) {
+                try lines.append(item);
+            const trimmed = std.mem.trim(u8, answer, " \t\n");
+            if (try matches(gpa, trimmed, item.def)) {
                 try stdout.writeAll("correct!\n");
                 try correct.append(item);
             } else {
@@ -72,4 +73,81 @@ pub fn main() anyerror!void {
 pub fn fatal(comptime format: []const u8, args: anytype) noreturn {
     std.log.emerg(format, args);
     std.process.exit(1);
+}
+
+// thanks @rafi for this
+fn lev(comptime T: type, allocator: *std.mem.Allocator, lhs: []const T, rhs: []const T) !usize {
+    // create two work vectors of integer distances
+    var v0 = try allocator.alloc(usize, rhs.len);
+    defer allocator.free(v0);
+    var v1 = try allocator.alloc(usize, rhs.len);
+    defer allocator.free(v1);
+
+    // initialize v0 (the previous row of distances)
+    // this row is A[0][i]: edit distance for an empty s
+    // the distance is just the number of characters to delete from t
+    var i: usize = 0;
+    while (i < rhs.len) : (i += 1) {
+        v0[i] = i;
+    }
+
+    i = 0;
+    while (i < lhs.len - 1) : (i += 1) {
+        // calculate v1 (current row distances) from the previous row v0
+
+        // first element of v1 is A[i+1][0]
+        //   edit distance is delete (i+1) chars from s to match empty t
+        v1[0] = i + 1;
+
+        // use formula to fill in the rest of the row
+        var j: usize = 0;
+        while (j < rhs.len - 1) : (j += 1) {
+            // calculating costs for A[i+1][j+1]
+            const deletionCost: usize = v0[j + 1] + 1;
+            const insertionCost: usize = v1[j] + 1;
+            const substitutionCost = blk: {
+                if (lhs[i] == rhs[j]) {
+                    break :blk v0[j];
+                } else {
+                    break :blk v0[j] + 1;
+                }
+            };
+
+            const minCost = blk: {
+                var min: usize = std.math.maxInt(usize);
+                if (deletionCost < min) {
+                    min = deletionCost;
+                }
+                if (insertionCost < min) {
+                    min = insertionCost;
+                }
+                if (substitutionCost < min) {
+                    min = substitutionCost;
+                }
+                break :blk min;
+            };
+            v1[j + 1] = minCost;
+        }
+
+        // copy v1 (current row) to v0 (previous row) for next iteration
+        // since data in v1 is always invalidated, a swap without copy could be more efficient
+        std.mem.copy(usize, v0, v1);
+    }
+    return v0[rhs.len - 1];
+}
+
+test "lev" {
+    var allocator = std.testing.allocator;
+    const dist = try lev(u8, allocator, "kitten", "sitting");
+    std.testing.expect(dist == 3);
+}
+
+pub fn matches(ally: *std.mem.Allocator, guess: []const u8, ans: []const u8) !bool {
+    var min: usize = std.math.maxInt(usize);
+    var possible = std.mem.tokenize(ans, ";,");
+    while (possible.next()) |item| {
+        const num: usize = try lev(u8, ally, guess, item);
+        if (num < min) min = num;
+    }
+    return min < 15;
 }
